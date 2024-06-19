0782d08fbb83c654156fcff2347a5dda0a35597f
MaxNollet <55237749+MaxNollet@users.noreply.github.com>
2024-06-19 21:17:57 +0200

    Reworked and isolated preprocessing of ARSIA data.

--- src/barometer/lab/arsia.py
+++ src/barometer/lab/arsia.py
@@ -3,22 +3,25 @@ import hashlib
 import numpy as np
 import pandas as pd
 import rdflib
-from rdflib import Graph, Namespace
+from pandas import DataFrame
 from rdflib import Literal
+from rdflib import Namespace
 from rdflib.namespace import XSD
 
 
-def process_files(files) -> str:
-    # Load the data into Pandas dataframes
-    dfs = []
-    for file in files:
-        df = pd.read_excel(file, engine="openpyxl")
-        dfs.append(df)
+def preprocess(dataframe_raw: DataFrame) -> DataFrame:
+    """Preprocess data into report-ready format.
 
-    barometer_dt_raw = dfs[0]
+    The data provided was in an awkward format and wasn't suitable for
+    reporting-purposes. This function makes data anonym, molds existing
+    columns into multiple usable columns and shapes the data in a report-ready
+    format.
 
+    :param dataframe_raw: original, unprocessed data.
+    :return: preprocessed data, ready for reporting.
+    """
     # Rename columns
-    barometer_dt_raw.rename(
+    dataframe_raw.rename(
         columns={
             "N° échantillon": "Dossier",
             "Date of Sample": "Date",
@@ -37,51 +40,49 @@ def process_files(files) -> str:
     )
 
     # Separate ADDRESS column into Postal_code and City
-    barometer_dt_raw[["Postal_code", "City"]] = barometer_dt_raw[
+    dataframe_raw[["Postal_code", "City"]] = dataframe_raw[
         "ADDRESS"
     ].str.split(n=1, expand=True)
-
     # Convert Postal_code to numeric
-    barometer_dt_raw["Postal_code"] = pd.to_numeric(
-        barometer_dt_raw["Postal_code"], errors="coerce"
+    dataframe_raw["Postal_code"] = pd.to_numeric(
+        dataframe_raw["Postal_code"], errors="coerce"
     )
-
     # Create new columns
-    barometer_dt_raw["FileNumber"] = barometer_dt_raw["Dossier"].str.slice(
+    dataframe_raw["FileNumber"] = dataframe_raw["Dossier"].str.slice(
         stop=12
     )
-    barometer_dt_raw["SampleNumber"] = barometer_dt_raw["Dossier"].str.slice(
+    dataframe_raw["SampleNumber"] = dataframe_raw["Dossier"].str.slice(
         start=-3
     )
-    barometer_dt_raw["Country"] = "Belgium"
-    barometer_dt_raw["LabReference"] = "3"
+    dataframe_raw["Country"] = "Belgium"
+    dataframe_raw["LabReference"] = "3"
 
     # Map Sample_type
     sample_type_mapping = {"BAL": "BAL", "SWAB": "Swab", "CARCASS": "Autopsy"}
-    barometer_dt_raw["SampleType"] = barometer_dt_raw["SampleType"].map(
+    dataframe_raw["SampleType"] = dataframe_raw["SampleType"].map(
         sample_type_mapping
     )
 
     # Map Breed
     breed_mapping = {"MEAT": "Beef", "MILK": "Dairy", "MXD": "Mixed"}
-    barometer_dt_raw["Breed"] = (
-        barometer_dt_raw["SPECUL"].map(breed_mapping).fillna("Unknown")
+    dataframe_raw["Breed"] = (
+        dataframe_raw["SPECUL"].map(breed_mapping).fillna("Unknown")
     )
 
     # Map Province based on Postal_code
     postal_code_conditions = [
-        (barometer_dt_raw["Postal_code"].between(1000, 1299)),
-        (barometer_dt_raw["Postal_code"].between(1300, 1499)),
-        (barometer_dt_raw["Postal_code"].between(1500, 1999)),
-        (barometer_dt_raw["Postal_code"].between(3000, 3499)),
-        (barometer_dt_raw["Postal_code"].between(2000, 2999)),
-        (barometer_dt_raw["Postal_code"].between(3500, 3999)),
-        (barometer_dt_raw["Postal_code"].between(4000, 4999)),
-        (barometer_dt_raw["Postal_code"].between(5000, 5999)),
-        (barometer_dt_raw["Postal_code"].between(6000, 6599)),
-        (barometer_dt_raw["Postal_code"].between(7000, 7999)),
-        (barometer_dt_raw["Postal_code"].between(6600, 6999)),
-        (barometer_dt_raw["Postal_code"].between(8000, 8999)),
+        (dataframe_raw["Postal_code"].between(1000, 1299)),
+        (dataframe_raw["Postal_code"].between(1300, 1499)),
+        (dataframe_raw["Postal_code"].between(1500, 1999)),
+        (dataframe_raw["Postal_code"].between(3000, 3499)),
+        (dataframe_raw["Postal_code"].between(2000, 2999)),
+        (dataframe_raw["Postal_code"].between(3500, 3999)),
+        (dataframe_raw["Postal_code"].between(4000, 4999)),
+        (dataframe_raw["Postal_code"].between(5000, 5999)),
+        (dataframe_raw["Postal_code"].between(6000, 6599)),
+        (dataframe_raw["Postal_code"].between(7000, 7999)),
+        (dataframe_raw["Postal_code"].between(6600, 6999)),
+        (dataframe_raw["Postal_code"].between(8000, 8999)),
     ]
     province_choices = [
         "Brussels",
@@ -97,7 +98,7 @@ def process_files(files) -> str:
         "Luxembourg",
         "West Flanders",
     ]
-    barometer_dt_raw["Province"] = pd.Series(
+    dataframe_raw["Province"] = pd.Series(
         pd.Categorical(
             np.select(
                 postal_code_conditions,
@@ -108,7 +109,7 @@ def process_files(files) -> str:
     )
 
     # Select columns
-    barometer_dt = barometer_dt_raw[
+    dataframe = dataframe_raw[
         [
             "FileNumber",
             "DiagnosticTest",
@@ -132,30 +133,28 @@ def process_files(files) -> str:
     ].copy()
 
     # Drop duplicates
-    barometer_dt.drop_duplicates(inplace=True)
+    dataframe.drop_duplicates(inplace=True)
 
     # Hash Filenumber, Samplenumber, and Farm_ID
-    barometer_dt["FileNumber"] = barometer_dt["FileNumber"].apply(
+    dataframe["FileNumber"] = dataframe["FileNumber"].apply(
         lambda x: hashlib.sha256(str(x).encode()).hexdigest()
     )
-    barometer_dt["SampleNumber"] = barometer_dt["SampleNumber"].apply(
+    dataframe["SampleNumber"] = dataframe["SampleNumber"].apply(
         lambda x: hashlib.sha256(str(x).encode()).hexdigest()
     )
-    barometer_dt["FarmID"] = barometer_dt["FarmID"].apply(
+    dataframe["FarmID"] = dataframe["FarmID"].apply(
         lambda x: hashlib.sha256(str(x).encode()).hexdigest()
     )
-
-    # Floor date to 1st of month
-    # barometer_dt['Floored_date'] = pd.to_datetime(barometer_dt['Date']).dt.to_period('M').dt.to_timestamp()
 
     # Add a floored_date column
-    barometer_dt["Floored_date"] = barometer_dt["Date"].apply(
+    dataframe["Floored_date"] = dataframe["Date"].apply(
         lambda x: x - pd.to_timedelta(x.day - 1, unit="d")
     )
 
     # Group and aggregate data
-    barometer_groupby = (
-        barometer_dt.groupby(
+    columns_to_aggregate = ["PM", "MH", "HS", "MB", "BRSV", "PI3", "BCV"]
+    barometer_grouped = (
+        dataframe.groupby(
             [
                 "LabReference",
                 "Country",
@@ -167,20 +166,34 @@ def process_files(files) -> str:
                 "SampleType",
             ],
             observed=True,
-        )
-        .agg(
-            {
-                "PM": "max",
-                "MH": "max",
-                "HS": "max",
-                "MB": "max",
-                "BRSV": "max",
-                "PI3": "max",
-                "BCV": "max",
-            }
-        )
+        )[columns_to_aggregate]
+        .max()
         .reset_index()
     )
+
+    # Convert to long format
+    pathogens = ['PM', 'MH', 'HS', 'MB', 'BRSV', 'PI3', 'BCV']
+    barometer_long = pd.melt(
+        barometer_grouped,
+        value_vars=pathogens,
+        var_name='Pathogen',
+        value_name='Result'
+    )
+
+    return barometer_long
+
+
+def process_files(files) -> str:
+    # Load the data into Pandas dataframes
+    dfs = []
+    for file in files:
+        df = pd.read_excel(file, engine="openpyxl")
+        dfs.append(df)
+
+    barometer_dt_raw = dfs[0]
+
+    # Floor date to 1st of month
+    # barometer_dt['Floored_date'] = pd.to_datetime(barometer_dt['Date']).dt.to_period('M').dt.to_timestamp()
 
     # Convert to long format
     # pathogens = ['PM', 'MH', 'HS', 'MB', 'BRSV', 'PI3', 'BCV']
@@ -234,6 +247,7 @@ def process_files(files) -> str:
         value_name="Result",
     )
 
+    # Graph creation
     g = rdflib.Graph()
     onto = Namespace("http://www.purl.org/decide/LivestockHealthOnto")
     g.bind("onto", onto)
